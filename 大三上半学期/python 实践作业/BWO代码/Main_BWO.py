9e3b24c640b2c1fb298e422ec63b23a680e7f744
Jerry Huang <17321421572@qq.com>
2024-12-20 10:08:13 +0800

    提交作业

--- 大三上半学期/python 实践作业/BWO代码/Main_BWO.py
+++ 大三上半学期/python 实践作业/BWO代码/Main_BWO.py
@@ -7,10 +7,11 @@ import copy
 import benchmarks
 import BWO
 import time
+import BWO_combine
 
 ''' --------------------------- 参数设置 ----------------------------------'''
 SearchAgents_no = 50  # 种群量级
-Function_name = 'F7'  # 测试函数
+Function_name = 'F9'  # 测试函数
 Max_iteration = 500   # 迭代次数
 
 ''' ------------------------ 获取测试函数细节 F1~F23 ----------------------------------'''
@@ -21,17 +22,43 @@ ub = func_details1[2]
 dim = func_details1[3]
 fobj = getattr(benchmarks, Function_name) # 获取函数求解
 
-''' ------------------------ 白鲸求解 ----------------------------------'''
-begin_time=time.time()
-x = BWO.BWO(fobj, lb, ub, dim, SearchAgents_no, Max_iteration)
-print(f"在第{x.bestTime}次迭代时找到最优解")
-print(f"本次迭代所用的时间为：{abs(begin_time-x.endTime)}")
-''' ------------------------ 求解结果 ----------------------------------'''
-IterCurve = x.convergence
-Best_fitness = x.best
-Best_Pos = x.bestIndividual
 
-''' ------------------------ 绘图 ----------------------------------'''
+
+# ''' ------------------------ 白鲸求解 ----------------------------------'''
+# begin_time=time.time()
+# x = DO_BWO.BWO(fobj, lb, ub, dim, SearchAgents_no, Max_iteration)
+# print(f"在第{x.bestTime}次迭代时找到最优解")
+# print(f"本次迭代所用的时间为：{abs(begin_time-x.endTime)}")
+# ''' ------------------------ 求解结果 ----------------------------------'''
+# IterCurve = x.convergence
+# Best_fitness = x.best
+# Best_Pos = x.bestIndividual
+#
+# ''' ------------------------ 绘图 ----------------------------------'''
+# part1 = ['DO_BWO', Function_name]
+# name1 = ' '.join(part1)
+# plt.figure(1)
+# plt.semilogy(IterCurve, 'r-', linewidth=2)
+# plt.xlabel('Iteration', fontsize='medium')
+# plt.ylabel("Fitness", fontsize='medium')
+# plt.grid()
+# plt.title(name1, fontsize='large')
+# label = [name1]
+# plt.legend(label, loc='upper right')
+# plt.savefig('./BWO_Python.jpg')
+# plt.show()
+#
+# ''' ------------------------ 白鲸求解 ----------------------------------'''
+# begin_time=time.time()
+# x = BWO.BWO(fobj, lb, ub, dim, SearchAgents_no, Max_iteration)
+# print(f"在第{x.bestTime}次迭代时找到最优解")
+# print(f"本次迭代所用的时间为：{abs(begin_time-x.endTime)}")
+# ''' ------------------------ 求解结果 ----------------------------------'''
+# IterCurve = x.convergence
+# Best_fitness = x.best
+# Best_Pos = x.bestIndividual
+#
+# ''' ------------------------ 绘图 ----------------------------------'''
 # part1 = ['BWO', Function_name]
 # name1 = ' '.join(part1)
 # plt.figure(1)
@@ -45,12 +72,15 @@ Best_Pos = x.bestIndividual
 # plt.savefig('./BWO_Python.jpg')
 # plt.show()
 
-
-time_itera=3
+time_itera=20
 
 best_arr =np.zeros(time_itera)
 best_time_arr = np.zeros(time_itera)
 useTime_arr = np.zeros(time_itera)
+
+best_arr_DOBWO = np.zeros(time_itera)
+best_time_DOBWO = np.zeros(time_itera)
+usetime_DOBWO = np.zeros(time_itera)
 # output_arr =[1,2,3,4,5]
 
 for i in range(time_itera):
@@ -60,40 +90,52 @@ for i in range(time_itera):
     print(x.best)
     best_arr[i] = x.best
     best_time_arr[i] = x.bestTime
-    useTime_arr[i] = begin_time-x.endTime
-    ''' ------------------------ 求解结果 ----------------------------------'''
-    IterCurve = x.convergence
-    Best_fitness = x.best
-    Best_Pos = x.bestIndividual
+    useTime_arr[i] = x.endTime - begin_time
 
-    ''' ------------------------ 绘图 ----------------------------------'''
-    part1 = ['BWO', Function_name]
-    name1 = ' '.join(part1)
-    plt.figure(1)
-    plt.semilogy(IterCurve, 'r-', linewidth=2)
-    plt.xlabel('Iteration', fontsize='medium')
-    plt.ylabel("Fitness", fontsize='medium')
-    plt.grid()
-    plt.title(name1, fontsize='large')
-    label = [name1]
-    plt.legend(label, loc='upper right')
-    plt.savefig('./BWO_Python.jpg')
-    plt.show()
+    begin_time2 = time.time()
+    x = DO_BWO.BWO(fobj, lb, ub, dim, SearchAgents_no, Max_iteration)
+    best_arr_DOBWO[i] = x.best
+    best_time_DOBWO[i] = x.bestTime
+    usetime_DOBWO[i] = x.endTime - begin_time2
+    # ''' ------------------------ 求解结果 ----------------------------------'''
+    # IterCurve = x.convergence
+    # Best_fitness = x.best
+    # Best_Pos = x.bestIndividual
+
+    # ''' ------------------------ 绘图 ----------------------------------'''
+    # part1 = ['BWO', Function_name]
+    # name1 = ' '.join(part1)
+    # plt.figure(1)
+    # plt.semilogy(IterCurve, 'r-', linewidth=2)
+    # plt.xlabel('Iteration', fontsize='medium')
+    # plt.ylabel("Fitness", fontsize='medium')
+    # plt.grid()
+    # plt.title(name1, fontsize='large')
+    # label = [name1]
+    # plt.legend(label, loc='upper right')
+    # plt.savefig('./BWO_Python.jpg')
+    # plt.show()
 
 print(best_arr)
 print(best_time_arr)
 print(useTime_arr)
 
+print(best_arr_DOBWO)
+print(best_time_DOBWO)
+print(usetime_DOBWO)
+
+
 mean_value = np.mean(best_arr)
-print(mean_value)
+print(f"BWO算法求解的中位数为：{np.median(best_arr)}")
+print(f"BWO算法求解的方差为：{np.var(best_arr)}")
+print(f"BWO算法完成500次迭代平均用时为：{np.mean(useTime_arr)}")
+print(f"BWO算法平均在{np.mean(best_time_arr)}次迭代时找到最优解")
+print("/n")
+print(f"DO_BWO算法求解的中位数为：{np.median(best_arr_DOBWO)}")
+print(f"DO_BWO算法求解的方差为：{np.var(best_arr_DOBWO)}")
+print(f"DO_BWO算法完成500次迭代平均用时为：{np.mean(usetime_DOBWO)}")
+print(f"DO_BWO算法平均在{np.mean(best_time_DOBWO)}次迭代时找到最优解")
 
-squa_diff = 0
-temp = 0
-for i in range(len(best_arr)):
-    squa_diff = (best_arr[i]-mean_value)*(best_arr[i]-mean_value)
-    temp = squa_diff+temp
 
-squa_diff = temp/time
-print(squa_diff)
 
 
---
+++ 大三上半学期/测量系统/assignment_md/Lab2_Part3.md
@@ -0,0 +1,55 @@
+# <center> Fresquency Response </center>
+<center>Jairui Huang(黄家睿)</center>
+<center>202283890036</center>
+
+## Introduction and Aim
+This experiment investigates the frequency response of a 100x gain amplifier. The
+primary objective is to measure how gain varies with frequency, determine the amplifier’s bandwidth, and analyze the gain-bandwidth trade-off to ensure optimal performance.
+
+## Theory
+The experiment is based on
+the gain-bandwidth product (GBW) principle of operational amplifiers. For a 741
+operational amplifier, the relationship governs its frequency response. As the frequency increases, the gain decreases in accordance with this principle. The frequency
+response curve, which plots gain against frequency, highlights key performance parameters such as the amplifier’s bandwidth and the frequency at which gain begins
+to roll off.
+Relevant equations include:
+where is the gain, is the output voltage, and is the input voltage.
+
+## Experimental Method and Results
+
+### Circuit Diagram
+<div style="text-align: center;">
+    <img src="../Lab_picture/Lab7_circuitDiagram.png" alt="Signal Diagram" width="400" />
+</div>
+
+### Experiment Method
+The experiment was carried out by:
+Configuring the amplifier circuit with a 100x gain.
+Applying a sinusoidal input signal with a constant amplitude of 50 mV from the
+function generator.
+Gradually increasing the signal frequency from 50 Hz to 100 kHz and recording
+the corresponding output voltage using an oscilloscope.
+Ensuring proper grounding, impedance matching, and signal shielding to minimize interference and inaccuracies.
+
+### Data table
+
+
+|Frequency (Hz)| Output Voltage Vout (V)| Gain Av = $\frac{V_{\text{out}}}{V_{\text{in}}}$|
+|---|---|---|
+|50|3.10|15.5|
+|60|2.6|13|
+|70|2.22|11.1|
+|80|1.95|9.75|
+|90|1.73|8.65|
+|100|1.55|7.75|
+
+### The Graphical Analysis
+The frequency response curve (Figure 2) shows the gain decreasing as frequency
+increases, with a clear roll-off beyond the cutoff frequency. The bandwidth, defined
+as the frequency at which the gain falls to of its maximum value, was determined to
+be approximately [specific value] kHz.
+
+
+<div style="text-align: center;">
+    <img src="../Lab_picture/Lab7_diagram.png" alt="Signa Diagram" width="400" />
+</div>
